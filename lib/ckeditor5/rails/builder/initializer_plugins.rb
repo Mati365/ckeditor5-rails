b73128d23995aa91decdcdc234e5c1a50c7319e7
Mateusz Baginski <cziken58@gmail.com>
2024-10-25 15:09:03 +0200

    Adjust more shit

--- lib/ckeditor5/rails/builder/initializer_plugins.rb
+++ lib/ckeditor5/rails/builder/initializer_plugins.rb
@@ -9,56 +9,63 @@ module CKEditor5::Rails::Builder
     end
 
     def js_config_plugins
-      return @js_config_plugins if defined?(@js_config_plugins)
+      @js_config_plugins ||= generate_js_config_plugins
+    end
 
-      plugins = [
+    def js_imports
+      @js_imports ||= generate_js_imports
+    end
+
+    private
+
+    def generate_js_config_plugins
+      plugins_list = [
         *internal_plugins,
         *window_plugins.map { |plugin| plugin[:name] },
         *esm_plugins.map { |plugin| plugin[:import_name] }
       ]
 
-      @js_config_plugins = "[ #{plugins.join(', ')} ]".delete('"')
+      "[ #{plugins_list.join(', ')} ]".delete('"')
     end
 
-    def js_imports
-      @js_imports ||= [
-        internal_plugins.empty? ? '' : "import { #{internal_plugins.join(', ')} } from 'ckeditor5';",
-        esm_plugins.map { |plugin| "import { #{plugin[:import_name]} } from '#{plugin[:name]}';" },
-        window_plugins.map { |plugin| "const #{plugin[:name]} = window['#{plugin[:name]}'];" }
+    def generate_js_imports
+      [
+        internal_plugins_import,
+        esm_plugins_imports,
+        window_plugins_imports
       ].join("\n")
     end
 
-    private
+    def internal_plugins_import
+      return '' if internal_plugins.empty?
+
+      "import { #{internal_plugins.join(', ')} } from 'ckeditor5';"
+    end
+
+    def esm_plugins_imports
+      esm_plugins.map { |plugin| "import { #{plugin[:import_name]} } from '#{plugin[:name]}';" }.join("\n")
+    end
+
+    def window_plugins_imports
+      window_plugins.map { |plugin| "const #{plugin[:name]} = window['#{plugin[:name]}'];" }.join("\n")
+    end
 
     def internal_plugins
-      @internal_plugins ||= plugins.filter_map do |plugin|
-        next unless plugin.is_a?(String) || plugin.is_a?(Symbol)
-
-        plugin.to_s
-      end
+      @internal_plugins ||= plugins.select do |plugin|
+        plugin.is_a?(String) || plugin.is_a?(Symbol)
+      end.map(&:to_s)
     end
 
     def window_plugins
-      @window_plugins ||= plugins.filter_map do |plugin|
-        next unless plugin.is_a?(CustomWindowPlugin)
-
+      @window_plugins ||= plugins.select { |plugin| plugin.is_a?(CustomWindowPlugin) }.map do |plugin|
         name = "__window_plugin_#{plugin.name.parameterize}"
-
-        {
-          code: "const #{name} = window['#{plugin.name}'];",
-          name: name
-        }
+        { code: "const #{name} = window['#{plugin.name}'];", name: name }
       end
     end
 
     def esm_plugins
-      @esm_plugins ||= plugins.filter_map do |plugin|
-        next unless plugin.is_a?(CustomEsmPlugin)
-
-        {
-          import_name: plugin.import_name,
-          name: plugin.name
-        }
+      @esm_plugins ||= plugins.select { |plugin| plugin.is_a?(CustomEsmPlugin) }.map do |plugin|
+        { import_name: plugin.import_name, name: plugin.name }
       end
     end
   end
