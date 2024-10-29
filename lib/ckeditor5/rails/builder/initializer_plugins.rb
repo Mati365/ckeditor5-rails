100dabb4328e7e04f78581216e4d668b02947491
Mateusz Baginski <cziken58@gmail.com>
2024-10-29 11:40:40 +0100

    Better split classes

--- lib/ckeditor5/rails/builder/initializer_plugins.rb
+++ lib/ckeditor5/rails/builder/initializer_plugins.rb
@@ -6,6 +6,9 @@ module CKEditor5::Rails::Builder
 
     def initialize(plugins)
       @plugins = plugins || []
+      @internal_handler = InternalPluginsHandler.new(@plugins)
+      @window_handler = WindowPluginsHandler.new(@plugins)
+      @esm_handler = EsmPluginsHandler.new(@plugins)
     end
 
     def js_config_plugins
@@ -20,9 +23,9 @@ module CKEditor5::Rails::Builder
 
     def generate_js_config_plugins
       plugins_list = [
-        *internal_plugins,
-        *window_plugins.map { |plugin| plugin[:name] },
-        *esm_plugins.map { |plugin| plugin[:import_name] }
+        *@internal_handler.internal_plugins,
+        *@window_handler.window_plugins.map { |plugin| plugin[:name] },
+        *@esm_handler.esm_plugins.map { |plugin| plugin[:import_name] }
       ]
 
       "[ #{plugins_list.join(', ')} ]".delete('"')
@@ -30,34 +33,34 @@ module CKEditor5::Rails::Builder
 
     def generate_js_imports
       [
-        internal_plugins_import,
-        esm_plugins_imports,
-        window_plugins_imports
+        @internal_handler.js_imports,
+        @esm_handler.js_imports,
+        @window_handler.js_imports
       ].join("\n")
     end
+  end
 
-    def internal_plugins_import
-      return '' if internal_plugins.empty?
+  class InternalPluginsHandler
+    attr_reader :internal_plugins
 
-      "import { #{internal_plugins.join(', ')} } from 'ckeditor5';"
-    end
-
-    def esm_plugins_imports
-      esm_plugins.map { |plugin| "import { #{plugin[:import_name]} } from '#{plugin[:name]}';" }.join("\n")
-    end
-
-    def window_plugins_imports
-      window_plugins.map { |plugin| "const #{plugin[:name]} = window['#{plugin[:name]}'];" }.join("\n")
-    end
-
-    def internal_plugins
-      @internal_plugins ||= plugins.filter_map do |plugin|
+    def initialize(plugins)
+      @internal_plugins = plugins.filter_map do |plugin|
         plugin.to_s if plugin.is_a?(String) || plugin.is_a?(Symbol)
       end
     end
 
-    def window_plugins
-      @window_plugins ||= plugins.filter_map do |plugin|
+    def js_imports
+      return '' if internal_plugins.empty?
+
+      "import { #{internal_plugins.join(', ')} } from 'ckeditor5';"
+    end
+  end
+
+  class WindowPluginsHandler
+    attr_reader :window_plugins
+
+    def initialize(plugins)
+      @window_plugins = plugins.filter_map do |plugin|
         if plugin.is_a?(CustomWindowPlugin)
           name = "__window_plugin_#{plugin.name.parameterize}"
           { code: "const #{name} = window['#{plugin.name}'];", name: name }
@@ -65,10 +68,22 @@ module CKEditor5::Rails::Builder
       end
     end
 
-    def esm_plugins
-      @esm_plugins ||= plugins.filter_map do |plugin|
+    def js_imports
+      window_plugins.map { |plugin| "const #{plugin[:name]} = window['#{plugin[:name]}'];" }.join("\n")
+    end
+  end
+
+  class EsmPluginsHandler
+    attr_reader :esm_plugins
+
+    def initialize(plugins)
+      @esm_plugins = plugins.filter_map do |plugin|
         { import_name: plugin.import_name, name: plugin.name } if plugin.is_a?(CustomEsmPlugin)
       end
+    end
+
+    def js_imports
+      esm_plugins.map { |plugin| "import { #{plugin[:import_name]} } from '#{plugin[:name]}';" }.join("\n")
     end
   end
 
