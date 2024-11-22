f443832850b072027960f7cefd455c333f815550
Mateusz Baginski <cziken58@gmail.com>
2024-11-22 07:05:15 +0100

    Add more form tests

--- spec/lib/ckeditor5/rails/hooks/form_spec.rb
+++ spec/lib/ckeditor5/rails/hooks/form_spec.rb
@@ -42,6 +42,162 @@ RSpec.describe CKEditor5::Rails::Hooks::Form do
                                               })
         end
       end
+
+      context 'with as parameter' do
+        subject(:rendered_editor) do
+          builder.build_editor(:content, as: 'custom_field')
+        end
+
+        it 'uses custom field name' do
+          expect(rendered_editor).to have_tag('ckeditor-component', with: {
+                                                name: 'post[custom_field]'
+                                              })
+        end
+      end
+
+      context 'without object_name' do
+        let(:builder) { described_class.new('', post, template) }
+
+        it 'uses method name as field name' do
+          expect(rendered_editor).to have_tag('ckeditor-component', with: {
+                                                name: 'content'
+                                              })
+        end
+
+        context 'with as parameter' do
+          subject(:rendered_editor) do
+            builder.build_editor(:content, as: 'custom_field')
+          end
+
+          it 'uses as parameter as field name' do
+            expect(rendered_editor).to have_tag('ckeditor-component', with: {
+                                                  name: 'custom_field'
+                                                })
+          end
+        end
+      end
+
+      context 'with initial data handling' do
+        before do
+          allow(template).to receive(:ckeditor5_editor)
+        end
+
+        context 'when object responds to the method' do
+          it 'passes object method value as initial_data' do
+            builder.build_editor(:content)
+
+            expect(template).to have_received(:ckeditor5_editor)
+              .with(hash_including(initial_data: 'Initial content'))
+          end
+        end
+
+        context 'when object does not respond to the method' do
+          let(:post) { double('Post') }
+
+          it 'passes options initial_data value' do
+            builder.build_editor(:content, initial_data: 'Provided content')
+
+            expect(template).to have_received(:ckeditor5_editor)
+              .with(hash_including(initial_data: 'Provided content'))
+          end
+        end
+      end
+
+      context 'with validation classes handling' do
+        before do
+          allow(template).to receive(:ckeditor5_editor)
+        end
+
+        context 'when object has errors on the field' do
+          let(:post) do
+            instance_double('Post', errors: { content: ['is invalid'] })
+          end
+
+          it 'adds is-invalid class' do
+            builder.build_editor(:content, class: 'custom-class')
+
+            expect(template).to have_received(:ckeditor5_editor)
+              .with(hash_including(class: 'custom-class is-invalid'))
+          end
+
+          it 'adds is-invalid class when no initial class exists' do
+            builder.build_editor(:content)
+
+            expect(template).to have_received(:ckeditor5_editor)
+              .with(hash_including(class: 'is-invalid'))
+          end
+        end
+
+        context 'when object has no errors' do
+          let(:post) do
+            instance_double('Post', errors: {})
+          end
+
+          it 'keeps original class unchanged' do
+            builder.build_editor(:content, class: 'custom-class')
+
+            expect(template).to have_received(:ckeditor5_editor)
+              .with(hash_including(class: 'custom-class'))
+          end
+        end
+
+        context 'when object does not respond to errors' do
+          let(:post) { double('Post') }
+
+          it 'keeps original class unchanged' do
+            builder.build_editor(:content, class: 'custom-class')
+
+            expect(template).to have_received(:ckeditor5_editor)
+              .with(hash_including(class: 'custom-class'))
+          end
+        end
+      end
+    end
+  end
+
+  describe CKEditor5::Rails::Hooks::Form::FormBuilderExtension do
+    let(:template) { instance_double('ActionView::Base') }
+    let(:object) { double('Post') }
+    let(:builder) do
+      Class.new do
+        include CKEditor5::Rails::Hooks::Form::FormBuilderExtension
+        attr_reader :object_name, :object, :template
+
+        def initialize(object_name, object, template)
+          @object_name = object_name
+          @object = object
+          @template = template
+        end
+      end.new('post', object, template)
+    end
+
+    describe '#ckeditor5' do
+      let(:input_builder) { instance_double(CKEditor5::Rails::Hooks::Form::EditorInputBuilder) }
+
+      before do
+        allow(CKEditor5::Rails::Hooks::Form::EditorInputBuilder)
+          .to receive(:new)
+          .with('post', object, template)
+          .and_return(input_builder)
+        allow(input_builder).to receive(:build_editor)
+      end
+
+      it 'creates EditorInputBuilder with correct parameters' do
+        builder.ckeditor5(:content, class: 'custom-class')
+
+        expect(CKEditor5::Rails::Hooks::Form::EditorInputBuilder)
+          .to have_received(:new)
+          .with('post', object, template)
+      end
+
+      it 'calls build_editor with correct parameters' do
+        options = { class: 'custom-class' }
+        builder.ckeditor5(:content, options)
+
+        expect(input_builder)
+          .to have_received(:build_editor)
+          .with(:content, options)
+      end
     end
   end
 end
