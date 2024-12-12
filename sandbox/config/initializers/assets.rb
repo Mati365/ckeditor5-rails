ee451a3465fc77a06c90622ae6036a5b1f0f7f8e
Mateusz Baginski <cziken58@gmail.com>
2024-12-12 14:07:45 +0100

    Add images to demos

--- sandbox/config/initializers/assets.rb
+++ sandbox/config/initializers/assets.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('app/assets/images')
+Rails.application.config.assets.precompile += %w[cards/*.png]
